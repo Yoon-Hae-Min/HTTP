da13fd6a3996293e14b9d66d6acbf0b90d00df34
Yoon-Hae-Min <yunhatmi@naver.com>
2022-10-28 10:44:33 +0900

    feat: 프로젝트 기본 세팅

---
+++ src/pages/Main.js
@@ -0,0 +1,19 @@
+import {
+  AppBar,
+  Box,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+function Main() {
+  return (
+    <>
+      <Container maxWidth="sm"></Container>
+    </>
+  );
+}
+
+export default Main;
