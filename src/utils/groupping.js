4eafd2568c5ce24c3eb35667ffa2b35e4156ca27
Yoon-Hae-Min <yunhatmi@naver.com>
2022-11-05 16:46:00 +0900

    feat: 알고리즘 util함수 추가

---
+++ src/utils/groupping.js
@@ -0,0 +1,230 @@
+let Students = {
+  TeamCount: 4,
+
+  data: [
+    {
+      studentId: 201911111,
+      name: "AAA",
+      score: 100,
+    },
+    {
+      studentId: 201922222,
+      name: "BBB",
+      score: 100,
+    },
+    {
+      studentId: 201933333,
+      name: "CCC",
+      score: 60,
+    },
+    {
+      studentId: 201944444,
+      name: "DDD",
+      score: 50,
+    },
+    {
+      studentId: 201955555,
+      name: "EEE",
+      score: 26,
+    },
+    {
+      studentId: 201966666,
+      name: "FFF",
+      score: 20,
+    },
+    {
+      studentId: 201977777,
+      name: "GGG",
+      score: 10,
+    },
+    {
+      studentId: 201988888,
+      name: "HHH",
+      score: 9,
+    },
+    {
+      studentId: 201999999,
+      name: "III",
+      score: 8,
+    },
+    {
+      studentId: 201912121,
+      name: "JJJ",
+      score: 7,
+    },
+    {
+      studentId: 201923232,
+      name: "KKK",
+      score: 6,
+    },
+    {
+      studentId: 201934343,
+      name: "LLL",
+      score: 5,
+    },
+    {
+      studentId: 201945454,
+      name: "MMM",
+      score: 4,
+    },
+    {
+      studentId: 201956565,
+      name: "NNN",
+      score: 3,
+    },
+    {
+      studentId: 201967676,
+      name: "OOO",
+      score: 2,
+    },
+    {
+      studentId: 201978787,
+      name: "PPP",
+      score: 1,
+    },
+  ],
+};
+
+let TeamList = {
+  teamlist: [],
+};
+
+//팀 클래스
+class Team {
+  constructor() {
+    this.team = [];
+    this.teamscore = 0;
+    this.NumOfMem = 0;
+  }
+
+  addStudent(obj) {
+    this.team.push(obj);
+    this.teamscore += obj.score;
+    this.NumOfMem++;
+  }
+}
+
+//팀 최소힙 클래스
+class MinHeap {
+  constructor() {
+    this.teams = [];
+  }
+
+  //값을 서로 바꾸는 메소드
+  swap(index1, index2) {
+    let temp = this.teams[index1]; // items의 index1의 값을 temp(임시공간)에 담음
+    this.teams[index1] = this.teams[index2]; // index1에 index2의 값을 저장
+    this.teams[index2] = temp; // index2에 아까 index1의 값을 temp에 넣어놓은 값을 저장
+  }
+
+  //왼쪽 자식 인덱스 구하는 메소드
+  leftChildIndex(index) {
+    return index * 2 + 1;
+  }
+
+  //오른쪽 자식 인덱스 구하는 메소드
+  rightChildIndex(index) {
+    return index * 2 + 2;
+  }
+
+  //왼쪽 자식 노드 구하는 메소드
+  leftChild(index) {
+    return this.teams[this.leftChildIndex(index)];
+  }
+
+  //오른쪽 자식 노드 구하는 메소드
+  rightChild(index) {
+    return this.teams[this.rightChildIndex(index)];
+  }
+
+  //힙의 크기(항목 개수)를 반환하는 메소드
+  size() {
+    return this.teams.length;
+  }
+
+  //최소힙 정렬하기
+  heaping(index) {
+    if (this.leftChild(index) !== undefined) {
+      if (this.rightChild(index) !== undefined) {
+        if (
+          this.teams[index].teamscore > this.leftChild(index).teamscore ||
+          this.teams[index].teamscore > this.rightChild(index).teamscore
+        ) {
+          let smallerIndex =
+            this.leftChild(index).teamscore < this.rightChild(index).teamscore
+              ? this.leftChildIndex(index)
+              : this.rightChildIndex(index);
+          this.swap(index, smallerIndex);
+          this.heaping(smallerIndex);
+        }
+      } else {
+        if (this.teams[index].teamscore > this.leftChild(index).teamscore) {
+          let smallerIndex = this.leftChildIndex(index);
+          this.swap(index, smallerIndex);
+          this.heaping(smallerIndex);
+        }
+      }
+    }
+  }
+
+  //팀 추가
+  addTeam(obj) {
+    this.teams.unshift(obj);
+  }
+
+  //팀 스코어 가장 작은 팀에 학생 추가
+  addStu(obj) {
+    this.teams[0].addStudent(obj);
+  }
+
+  //팀 다 맞춰졌으면 뽑기
+  poll() {
+    this.teams[0] = this.teams.pop();
+  }
+}
+
+let sortStudents = Students.data.sort((a, b) => a.score - b.score); //배열 정렬
+
+//만들려고 하는 팀 수보다 학생수가 적으면 팀을 만들수 없어서
+//에러 처리를 해주었습니다. 콘솔이 아니라 팝업창을 띄운다던가 하면 더 좋을듯합니다
+let teamCount = Students.TeamCount;
+if (teamCount < sortStudents.lenght) console.log("팀을 만들 수 없습니다");
+
+let TeamHeap = new MinHeap();
+let overTeam = sortStudents.length % teamCount; //학생 수가 안맞을 때, 학생이 1명 더 많은 팀 수
+let NumOfMember = parseInt(sortStudents.length / teamCount); //팀당 학생 수
+
+//성적 가장 높은 학생 한명씩 넣기
+for (let i = 0; i < teamCount; i++) {
+  let team = new Team();
+  team.addStudent(sortStudents.pop());
+  TeamList.teamlist.unshift(team.team);
+  TeamHeap.addTeam(team);
+}
+
+//남은 학생 차례로 넣기
+let len = sortStudents.length;
+for (let i = 0; i < len; i++) {
+  if (overTeam > 0) {
+    if (TeamHeap.teams[0].NumOfMem == NumOfMember + 1) {
+      TeamHeap.poll();
+      overTeam--;
+      TeamHeap.heaping(0);
+      i--;
+    } else {
+      TeamHeap.heaping(0);
+      TeamHeap.addStu(sortStudents.pop());
+    }
+  } else {
+    if (TeamHeap.teams[0].NumOfMem == NumOfMember) {
+      TeamHeap.poll();
+      TeamHeap.heaping(0);
+      i--;
+    } else {
+      TeamHeap.heaping(0);
+      TeamHeap.addStu(sortStudents.pop());
+    }
+  }
+}
+
+console.log(TeamList);
