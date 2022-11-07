72c19c5049e54171584e8d1b23c5d8f5ea3c0594
Yoon-Hae-Min <yunhatmi@naver.com>
2022-11-07 19:17:00 +0900

    feat: 학생 수 보다 편성 인원이 많을 경우에 에러 메세지 popup

--- src/pages/StudentTeam.js
+++ src/pages/StudentTeam.js
@@ -9,6 +9,13 @@ const StudentTeam = () => {
   const { subjects, selectedSubject, dispatch } = useContext(InfoContexts);
 
   const onClickMakeTeam = () => {
+    if (
+      subjects[selectedSubject].numberOfTeams >
+      subjects[selectedSubject].students.length
+    ) {
+      alert("학생 수 보다 편성인원이 많습니다.");
+      return;
+    }
     const formatData = {
       teamCount: subjects[selectedSubject].numberOfTeams,
       data: JSON.parse(JSON.stringify(subjects[selectedSubject].students)), // 깊은복사를 해야한다 getTeams에서 불변성이 깨지기 때문에 state의 영향을 받아 student값이 지워지는 버그가 있었다.
